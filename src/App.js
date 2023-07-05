4700c8dd2c6ca3feb5b401fb4290f511baed17b9
EC2 Default User <ec2-user@ip-172-31-13-54.ap-northeast-2.compute.internal>
2023-07-05 05:42:50 +0000

    yoni blog

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS Yoni Blog
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
